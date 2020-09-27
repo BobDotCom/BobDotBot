1fbf667a976917ab1e5463f6f1d80bfcc1bb48af
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-09-27 17:05:36 -0500

    Update userdatacog.py

--- cogs/userdatacog.py
+++ cogs/userdatacog.py
@@ -24,53 +24,165 @@ class Levels(commands.Cog, name = "User database"):
     def __init__(self, client):
         self.client = client
         self.client.owner_id = 690420846774321221
+        global onreadyblocker
+        onreadyblocker = False
 
-
-        with open("users.json", 'r') as f:
-            self.users = json.load(f)
-
-
-        self.client.loop.create_task(self.save_users())
-
-    async def save_users(self):
-        await self.client.wait_until_ready()
-        while not self.client.is_closed():
-            json.dump(self.users, open("users.json", "w"), indent=4)
-
-
-            await asyncio.sleep(60)
-
-    def lvl_up(self, author_id):
-        cur_xp = self.users[author_id]['exp']
-        cur_lvl = self.users[author_id]['level']
-
-        if cur_xp >= round((4 * (cur_lvl ** 3)) / 7):
-            self.users[author_id]['level'] += 1
-            return True
-        else:
-            return False
     @commands.Cog.listener()
     async def on_message(self, message):
+      global onreadyblocker
+      if onreadyblocker == False:
+        onreadyblocker = True
+        if True:
+          def create_connection(path):
+            connection = None
+            try:
+              connection = sqlite3.connect(path)
+            except Error as e:
+              cprint(f"The error '{e}' occurred, clearing the database file will erase all data, but will make this script useable", 'red')
+            return connection
+          connection = create_connection("Users.db")
+          def execute_query(connection, query):
+            cursor = connection.cursor()
+            try:
+                cursor.execute(query)
+                connection.commit()
+            except Error as e:
+                cprint(f"The error '{e}' occurred, clearing the database file will erase all data, but will make this script useable", 'red')
+          def execute_read_query(connection, query):
+            cursor = connection.cursor()
+            result = None
+            try:
+              cursor.execute(query)
+              result = cursor.fetchall()
+              return result
+            except Error as e:
+              cprint(f"The error '{e}' occurred, clearing the database file will erase all data, but will make this script useable", 'red')
+          create_users_table = """
+          CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            userid TEXT NOT NULL,
+            level INTEGER NOT NULL,
+            xp INTEGER NOT NULL
+          );
+          """
+          execute_query(connection, create_users_table)
+        def get_userid(username):
+            try:
+              cursor = connection.cursor()
+        
+              sql_select_query = f"""select * from users where id = ?"""
+              cursor.execute(sql_select_query, (username,))
+              records = cursor.fetchall()
+        
+              for row in records:
+                return row[1]
+              cursor.close()
+            except Error as error:
+              print("Failed to read data from sqlite table", error)
+            finally:
+              if not (connection):
+                  connection.close()
+                  print("The SQLite connection is closed")
+        def get_id(info1):
+            try:
+              cursor = connection.cursor()
+        
+              sql_select_query = f"""select id from users where userid = ?"""
+              cursor.execute(sql_select_query, (info1,))
+              records = cursor.fetchall()
+        
+              for id in records:
+                return int(''.join(map(str, id)))
+              cursor.close()
+            except Error as error:
+              print("Failed to read data from sqlite table", error)
+            finally:
+              if not (connection):
+                  connection.close()
+                  print("The SQLite connection is closed")
+        def get_xp(username):
+            try:
+              cursor = connection.cursor()
+        
+              sql_select_query = f"""select * from users where id = ?"""
+              cursor.execute(sql_select_query, (username,))
+              records = cursor.fetchall()
+        
+              for row in records:
+                return row[3]
+              cursor.close()
+            except Error as error:
+              print("Failed to read data from sqlite table", error)
+            finally:
+              if not (connection):
+                  connection.close()
+                  print("The SQLite connection is closed")
+        def get_level(username):
+            try:
+              cursor = connection.cursor()
+        
+              sql_select_query = f"""select * from users where id = ?"""
+              cursor.execute(sql_select_query, (username,))
+              records = cursor.fetchall()
+        
+              for row in records:
+                return row[2]
+              cursor.close()
+            except Error as error:
+              print("Failed to read data from sqlite table", error)
+            finally:
+              if not (connection):
+                  connection.close()
+                  print("The SQLite connection is closed")
         if message.author == self.client.user:
             return
         if message.author.bot: return
         author_id = str(message.author.id)
-        if author_id in self.users:
-            self.users[author_id]['exp'] += 1
+        the_id = get_id(author_id)
+        userid = get_userid(the_id)
+        if userid != None:
+            xp = get_xp(the_id)
+            newxp = xp + 1
+            update_xp = f"""
+            UPDATE
+              users
+            SET
+              xp = {newxp}
+            WHERE
+              id = {the_id}
+            """
+            execute_query(connection, update_xp)
         else:
-            self.users[author_id] = {}
-            self.users[author_id]['level'] = 1
-            self.users[author_id]['exp'] = 1
-
-
-
-        if self.lvl_up(author_id):
-            await message.channel.send(f"{message.author.mention} is now level {self.users[author_id]['level']}")
-
+            create_users = f"""
+            INSERT INTO
+                users (userid, level, xp)
+            VALUES
+                ('{author_id}', 1, 1);
+            """
+            execute_query(connection, create_users)
+        the_id = get_id(author_id)
+        xp = get_xp(the_id)
+        level = get_level(the_id)
+        if xp >= round((4 * (level ** 3)) / 7):
+            newlevel = level + 1
+            update_level = f"""
+            UPDATE
+              users
+            SET
+              level = {newlevel}
+            WHERE
+              id = {the_id}
+            """
+            execute_query(connection, update_level)
+            msg = await message.channel.send(f"{message.author.mention} is now level {self.users[author_id]['level']}")
+            await asyncio.sleep(10)
+            await msg.delete()
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('UserDataCog is active')
+      print('UserDataCog is active')
+      
+              
 
 def setup(client):
     client.add_cog(Levels(client))
