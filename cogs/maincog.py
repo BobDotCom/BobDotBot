bc4f53320e8dfb25c3e64c223a15e0b6a38d4437
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-02 20:21:51 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -417,6 +417,8 @@ class MainCog(commands.Cog, name = "General"):
                     commandthing = self.client.get_command(command)
                     splice = cog[0]
                     cog = splice[0].upper() + splice[1:].lower()
+                    splice1 = commandthing.name[0]
+                    commandname = splice1[0].upper() + splice1[1:].lower()
                     #printing commands of cog
                     """Command listing within a cog."""
                     found = False
@@ -438,7 +440,7 @@ class MainCog(commands.Cog, name = "General"):
                             aliases = str(commandthing.aliases)
                             if aliases == "[]":
                                 aliases = "None"
-                            halp=discord.Embed(title=commandthing.name+' Info', timestamp=ctx.message.created_at,description=commandthing.help, color=discord.Color.blurple())
+                            halp=discord.Embed(title=commandname+' Info', timestamp=ctx.message.created_at,description=commandthing.help, color=discord.Color.blurple())
                             halp.set_footer(text=f"Bot made by {owner}", icon_url=owner.avatar_url) #if you like to
                             halp.add_field(name="Aliases",value=aliases,inline=False)
                             halp.add_field(name="Category",value=commandthing.cog_name,inline=False)
