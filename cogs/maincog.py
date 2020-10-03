0b85fd2bd183c00b9b082c757f49c909422ca7d0
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-03 12:58:22 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -348,7 +348,7 @@ class MainCog(commands.Cog, name = "General"):
         fmt = '{0.mention}: {0}. {0.id}\n{0.name} joined at {0.joined_at} and has {1} roles.'
         owner = self.client.get_user(self.client.owner_id)
         embedVar = discord.Embed(title="User Info", timestamp=ctx.message.created_at, description="" + fmt.format(member, len(member.roles)-1), color=discord.Color.blurple())
-        embedVar.add_field(name="Roles",value='Use my roles command to get roles'))
+        embedVar.add_field(name="Roles",value='Use my roles command to get roles')
         embedVar.set_footer(text=f"Bot made by {owner}", icon_url=owner.avatar_url) #if you like to
         await ctx.send(embed=embedVar)
 
