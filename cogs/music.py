ef9dd479e8cc2a17356357a17542e79765fcdce3
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-06 16:59:03 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -522,6 +522,7 @@ class Music(commands.Cog):
         # Inverse boolean value to loop and unloop.
         ctx.voice_state.loop = not ctx.voice_state.loop
         await ctx.message.add_reaction('✅')
+        await ctx.send(f"Loop state is now: {ctx.voice_state.loop}")
 
     @commands.command(name='play', aliases=['p'])
     async def _play(self, ctx: commands.Context, *, search: str):
