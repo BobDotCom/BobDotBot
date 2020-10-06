f0ea0722e2da7f93f4a9a795c44081590ac7e5be
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-06 15:41:32 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -352,13 +352,6 @@ class Music(commands.Cog):
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
         await ctx.send('An error occurred: {}'.format(str(error)))
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.id != bot.user.id:
-            print(f"{message.guild}/{message.channel}/{message.author.name}>{message.content}")
-            if message.embeds:
-                print(message.embeds[0].to_dict())
-
     @commands.command(name='join', invoke_without_subcommand=True)
     async def _join(self, ctx: commands.Context):
         """Joins a voice channel."""
