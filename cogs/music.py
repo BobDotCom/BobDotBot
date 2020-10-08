29c77bd8be7c31521defe78f7c1bebed2c16bc9d
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-08 10:30:08 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -460,7 +460,7 @@ class Music(commands.Cog):
             return await ctx.send('Not playing any music right now...')
 
         voter = ctx.message.author
-        if voter == ctx.voice_state.current.requester or len(ctx.voice_client.channel.members) == 2 or member.guild_permissions.manage_guild::
+        if voter == ctx.voice_state.current.requester or len(ctx.voice_client.channel.members) == 2 or member.guild_permissions.manage_guild:
             await ctx.message.add_reaction('⏭')
             ctx.voice_state.skip()
 
