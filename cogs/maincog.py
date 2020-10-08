45b79bd4a47b95fa73066af12ce74cdee0247787
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-08 12:29:12 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -841,7 +841,7 @@ class MainCog(commands.Cog, name = "General"):
     @commands.cooldown(1, 1, commands.BucketType.channel)
     async def binary(self,ctx,*,arg):
         res = ''.join(format(ord(i), 'b') for i in arg)
-        embed = discord.Embed(color=discrd.Color.blurple, timestamp=ctx.message.created_at, title="Text to binary", description=res)
+        embed = discord.Embed(color=discord.Color.blurple, timestamp=ctx.message.created_at, title="Text to binary", description=res)
 
 def setup(client):
     client.add_cog(MainCog(client))
