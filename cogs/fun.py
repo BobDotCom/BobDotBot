e3a251a87e482c4fe54db5fce113c6d445096eea
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-13 15:15:58 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -27,7 +27,7 @@ class FunCog(commands.Cog, name = "Fun"):
       await sess.close()
     @commands.command()
     @commands.cooldown(1, 1, commands.BucketType.channel)
-    @commands.max_concurrency(5, per=BucketType.channel)
+    @commands.max_concurrency(1, per=BucketType.channel)
     async def chatbot(self,ctx,*,chat = None):
         """Start a chat with a bot. Once you send your first message
         Uses `chatbot <chat>`
