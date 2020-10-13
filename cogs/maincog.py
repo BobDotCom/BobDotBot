29edbe0cf86fd03d2d9aa5ba5e18ba76522f6a84
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-13 08:24:32 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -954,7 +954,7 @@ class MainCog(commands.Cog, name = "General"):
         await ctx.send(embed=aembed)
     @commands.command(name="mystbin")
     async def mystbin(self, ctx, data):
-      data = bytes(str(data), 'utf-8')
+      data = bytes(data, 'utf-8')
       async with aiohttp.ClientSession() as cs:
         async with cs.post('https://mystb.in/documents', data = data) as r:
           res = await r.json()
