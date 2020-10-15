4eb80f1eef5c8f05f1b08c646eb630d4921eff76
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-15 10:02:47 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -62,7 +62,7 @@ class FunCog(commands.Cog, name = "Fun"):
     @commands.cooldown(1, 1, commands.BucketType.channel)
     async def red_panda(self,ctx):
       async with ctx.typing():
-        await get_the_image(self,ctx,"red_Panda")
+        await get_the_image(self,ctx,"red_panda")
 
     @commands.command()
     @commands.cooldown(1, 1, commands.BucketType.channel)
