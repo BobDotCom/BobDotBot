aebda256e8ebb4561a5723ae47f3316b5f71188a
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-15 17:33:25 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -22,7 +22,6 @@ from discord.ext import commands
 from datetime import datetime
 from discord.ext.commands import MissingPermissions
 from discord.ext import menus
-from dotenv import loadenv
 from lxml import html
 load_dotenv()
 MONITOR_TOKEN = os.getenv("MONITOR_TOKEN")
