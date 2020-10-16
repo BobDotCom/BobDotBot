af5c5f68951c78e4f88a7365d14fc6572c242298
BobDotCom <calebharvey.yt@gmail.com>
2020-10-15 21:17:06 -0500

    changed way of getting OwOified lyrics

--- cogs/music.py
+++ cogs/music.py
@@ -662,7 +662,7 @@ class Music(commands.Cog):
     async def _cancerlyrics(self, ctx, *, title = None):
         if not title:
             title = ctx.voice_state.current.ret_lyric()
-        lyrics = await api.get_lyrics(title + "&cancer=true")
+        lyrics = await api.get_lyrics(title, owo=True)
         embed = discord.Embed(title=f"{lyrics.title} - {lyrics.author}(but its cancer)",description=lyrics.lyrics,url=lyrics.link,timestamp=ctx.message.created_at)
         try:
             try:
