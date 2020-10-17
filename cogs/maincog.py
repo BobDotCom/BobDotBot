b214d5708a5c60686422ea97ddcf6011f3374491
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-17 17:47:32 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -1098,7 +1098,7 @@ class MainCog(commands.Cog, name = "General"):
         results = google_search(
             query, my_api_key, my_cse_id, num=10)
         for result in results:
-            embed.add_field(name="search",description=results)
+            embed.add_field(name="search",value=results)
         await ctx.send(embed=embed)
 
 def setup(client):
