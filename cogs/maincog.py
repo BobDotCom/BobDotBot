0e1198dfee76930548e6001e56b45b64c8284ae5
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-17 20:50:02 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -1122,7 +1122,9 @@ class MainCog(commands.Cog, name = "General"):
         thisasdf = []
         for result in results:
             thisasdf += [result, ]
-        print(thisasdf[0][title])
+        await ctx.send(thisasdf[0]["title"])
+        await ctx.send(thisasdf[0]["link"])
+        await ctx.send(thisasdf[0]["snippet"])
         #m = MyMenu()
         #await m.start(ctx,asdf)
 
