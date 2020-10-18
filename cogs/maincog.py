36ec31ae5b8628607ea397175793f2615fd76eb2
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-18 14:38:11 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -1112,7 +1112,7 @@ class MainCog(commands.Cog, name = "General"):
         thisasdf = []
         for result in results:
             thisasdf += [result, ]
-        print(thisasdf[0]["pagemap"]["metatags"])
+        print("https" + thisasdf[0]["pagemap"]["metatags"]["og:url"])
         #await ctx.send(thisasdf[0]["title"])
         #await ctx.send(thisasdf[0]["link"])
         #await ctx.send(thisasdf[0]["snippet"])
