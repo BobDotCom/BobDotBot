b3d81713188ad7bd286b0d4fccd6eaa02b592c08
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-21 13:28:09 -0500

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -34,7 +34,7 @@ class Moderator(commands.Cog):
             embed.add_field(name="This user has been warned",
                             value=f"{self.warn_count[str(user)]} time(s)")
     @commands.command(aliases=['nick'])
-    @commands.has_guild_permissions(change_nickname=True)
+    @commands.has_guild_permissions(manage_members=True)
     async def nickname(self, ctx, member : discord.Member, *args):
         if member == None:
             await ctx.send('Give me a user dumbass')
