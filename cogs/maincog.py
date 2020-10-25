e8973adfbe52e54a924b8288320d0e27d607e403
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-10-25 14:04:18 -0500

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -37,6 +37,8 @@ from discord.ext import menus
 load_dotenv()
 MONITOR_TOKEN = os.getenv("MONITOR_TOKEN")
 SR_API_TOKEN = os.getenv("SR_API_TOKEN")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
 api = sr_api.Client(SR_API_TOKEN)
 # Create an instance of a class
 # Create an instance of a class
@@ -1129,8 +1131,8 @@ class MainCog(commands.Cog, name = "General"):
     @commands.command(name="google",aliases=["g"])
     @commands.cooldown(5, 600, commands.BucketType.channel)
     async def google(self,ctx,*,query):
-        my_api_key = "AIzaSyA5vrTzd9OHvXc09q7oK26wjLVA3K5Y3Xo"
-        my_cse_id = "8ffe6dda337341c4b"
+        my_api_key = GOOGLE_API_KEY
+        my_cse_id = GOOGLE_CSE_ID
         def google_search(search_term, api_key, cse_id, **kwargs):
             service = build("customsearch", "v1", developerKey=api_key)
             res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
