2030eab764ccc218323bc5be9c5b8dfc57c80cb6
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-11-04 16:37:13 -0600

    Update maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -44,6 +44,37 @@ GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
 api = sr_api.Client(SR_API_TOKEN)
 # Create an instance of a class
 # Create an instance of a class
+class SphinxObjectFileReader:
+    # Inspired by Sphinx's InventoryFileReader
+    BUFSIZE = 16 * 1024
+
+    def __init__(self, buffer):
+        self.stream = io.BytesIO(buffer)
+
+    def readline(self):
+        return self.stream.readline().decode('utf-8')
+
+    def skipline(self):
+        self.stream.readline()
+
+    def read_compressed_chunks(self):
+        decompressor = zlib.decompressobj()
+        while True:
+            chunk = self.stream.read(self.BUFSIZE)
+            if len(chunk) == 0:
+                break
+            yield decompressor.decompress(chunk)
+        yield decompressor.flush()
+
+    def read_compressed_lines(self):
+        buf = b''
+        for chunk in self.read_compressed_chunks():
+            buf += chunk
+            pos = buf.find(b'\n')
+            while pos != -1:
+                yield buf[:pos].decode('utf-8')
+                buf = buf[pos + 1:]
+                pos = buf.find(b'\n')
 class MySource(menus.ListPageSource):
     def __init__(self, data):
         super().__init__(data, per_page=1)
