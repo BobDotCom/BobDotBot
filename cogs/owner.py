e2ee5e1ca7d0000f3dd086ae85d5d90006ea1e8c
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-11-05 08:54:23 -0600

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -200,7 +200,7 @@ class OwnerCog(commands.Cog, name = "Owner"):
     @commands.is_owner()
     async def pip(self, ctx, *, code):
         cog = self.client.get_cog("Jishaku")
-        code = codeblock_converter("python3 -m pip install -U" + code)
+        code = codeblock_converter("python3 -m pip install -U " + code)
         await cog.jsk_shell(ctx, argument=code)
 def setup(client):
     client.add_cog(OwnerCog(client))
