5744f94a98fddc9581b025bf90559b62f85bc63e
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-11-11 14:59:48 -0600

    Update reddit.py

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -12,7 +12,7 @@ async def getSub(self, ctx, subreddit):
       if True:
         url = f"https://reddit.com/r/{subreddit}/random.json?limit=2"
         async with aiohttp.ClientSession() as session:
-          async with session.get(f"https://reddit.com/r/{subreddit}/hot.json?limit=1") as r:
+          async with session.get(f"https://reddit.com/r/{subreddit}/hot.json?limit=2") as r:
             res = await r.json()
             s = ""
             subredditDict = dict(res[0]['data']['children'][0]['data'])
