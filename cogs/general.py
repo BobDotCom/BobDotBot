bae2086713069d9f0fb2a2109aedf059103bbec9
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-11-18 18:25:21 -0600

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -683,8 +683,8 @@ class MainCog(commands.Cog, name = "General"):
     async def invite(self, ctx, bot: discord.Member = None, permissions = None):
         """Invite a bot to your server! Defaults to me, but you can mention another bot to get their invite too"""
         bot = bot or self.client.user
-        permissions = permissions.lower() if permissions.lower() in ["normal","admin"] else "admin" if bot == self.client.user else "normal"
-        permissions = 8 if permissions == "admin" else 0
+        permissions = permissions if permissions.lower() in ["normal","admin"] else "admin" if bot == self.client.user else "normal"
+        permissions = 8 if permissions.lower() == "admin" else 0
         owner = self.client.get_user(self.client.owner_id)
         embedvar = discord.Embed(title=f"Bot invite for: {bot.name}", timestamp=ctx.message.created_at, description=f"[Click Here]({str(discord.utils.oauth_url(bot.id, discord.Permissions(permissions=permissions), guild=ctx.guild))}")
         embedvar.set_footer(text=f"Bot made by {owner}", icon_url=owner.avatar_url) #if you like to
