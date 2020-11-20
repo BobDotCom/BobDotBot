7ce3f5e23794a6c57091d95a222c6876ab6dcd45
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2020-11-19 20:17:27 -0600

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -25,7 +25,7 @@ class MySource(menus.ListPageSource):
         embed = discord.Embed(title=f"Lyrics for {entries.title}",description=f"Page: {menu.current_page + 1}/{menu._source.get_max_pages()}",url=entries.link)
         embed.add_field(name=f'{entries.title} - {entries.author}' ,value=entries.content)
         try:
-            embed.set_thumbnail(url=lyrics.thumbnail)
+            embed.set_thumbnail(url=entries.thumbnail)
             return embed
         except:
             return embed
